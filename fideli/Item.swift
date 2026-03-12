8ce3e64d5113ae7114b2d6dd6962da8a10259491
pravinwijay <pravinwijay@gmail.com>
2026-03-12 21:01:43 +0100

    Initial Commit

---
+++ fideli/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  fideli
+//
+//  Created by Wijayasuntharam Pravin on 12/03/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
