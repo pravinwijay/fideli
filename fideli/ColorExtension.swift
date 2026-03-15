5b274d5886f39941540949ef3d8bcdfd4290d98e
pravinwijay <pravinwijay@gmail.com>
2026-03-15 22:35:29 +0100

    go go

---
+++ fideli/ColorExtension.swift
@@ -0,0 +1,8 @@
+//
+//  ColorExtension.swift
+//  fideli
+//
+//  Created by Wijayasuntharam Pravin on 15/03/2026.
+//
+
+import Foundation
---
+++ fideli/View/CardDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  CardDetailView.swift
+//  fideli
+//
+//  Created by Wijayasuntharam Pravin on 15/03/2026.
+//
+
+import SwiftUI
+
+struct CardDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardDetailView()
+}
